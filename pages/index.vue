e1e52aacd428ab38023ffd0182922d3f143943c1
olawale7040 <olawale7040@gmail.com>
2020-09-29 08:49:47 +0100

    Luqman Installed Hooper Slider

--- pages/index.vue
+++ pages/index.vue
@@ -58,56 +58,68 @@
           Our Product help your team work from anywhere delivering exceptional
           customer experiences.
         </div>
-        <div class="arrow-direction flex">
+        <!-- <div class="arrow-direction flex">
           <div class="arrow-left" style="margin-right: 60px" @click="previous">
             <img src="/img/left-arrow.svg" />
           </div>
           <div class="arrow-left" @click="next">
             <img src="/img/right-arrow.svg" />
           </div>
-        </div>
+        </div> -->
       </div>
-      <div id="slide" class="products-container sm:flex">
-        <div class="product-div">
-          <div class="product-top-line optima-top"></div>
-          <div class="product-desc">
-            <img src="/img/Optima-logo.svg" />
-            <div class="product-name">Optima</div>
-            <div class="product-explain">
-              An Integrated Cloud ERP for Accounting, Supply Chain and Unified
-              Commerce Solution.
+      <!-- Slider Started -->
+      <div class="products-container sm:flex">
+        <hooper :items-to-show="3">
+          <slide>
+            <div class="product-div">
+              <div class="product-top-line optima-top"></div>
+              <div class="product-desc">
+                <img src="/img/Optima-logo.svg" />
+                <div class="product-name">Optima</div>
+                <div class="product-explain">
+                  An Integrated Cloud ERP for Accounting, Supply Chain and
+                  Unified Commerce Solution.
+                </div>
+                <div class="product-learn-more">Learn More</div>
+                <button class="product-btn optima-btn">TRY FOR FREE</button>
+              </div>
             </div>
-            <div class="product-learn-more">Learn More</div>
-            <button class="product-btn optima-btn">TRY FOR FREE</button>
-          </div>
-        </div>
-        <div class="product-div">
-          <div class="product-top-line humana-top"></div>
-          <div class="product-desc">
-            <img src="/img/Humana-logo.svg" />
-            <div class="product-name">HumanaR</div>
-            <div class="product-explain">
-              A digital HR system for seamless HR Operations and Payroll
-              compliance.
+          </slide>
+          <slide>
+            <div class="product-div">
+              <div class="product-top-line humana-top"></div>
+              <div class="product-desc">
+                <img src="/img/Humana-logo.svg" />
+                <div class="product-name">HumanaR</div>
+                <div class="product-explain">
+                  A digital HR system for seamless HR Operations and Payroll
+                  compliance.
+                </div>
+                <div class="product-learn-more">Learn More</div>
+                <button class="product-btn humana-btn">TRY FOR FREE</button>
+              </div>
             </div>
-            <div class="product-learn-more">Learn More</div>
-            <button class="product-btn humana-btn">TRY FOR FREE</button>
-          </div>
-        </div>
-        <div class="product-div">
-          <div class="product-top-line dome-top"></div>
-          <div class="product-desc">
-            <img src="/img/Dome-logo.svg" />
-            <div class="product-name">Dome</div>
-            <div class="product-explain">
-              A cloud file storage and collaboration tool. Keep track of changes
-              and restrict access.
+          </slide>
+          <slide>
+            <div class="product-div">
+              <div class="product-top-line dome-top"></div>
+              <div class="product-desc">
+                <img src="/img/Dome-logo.svg" />
+                <div class="product-name">Dome</div>
+                <div class="product-explain">
+                  A cloud file storage and collaboration tool. Keep track of
+                  changes and restrict access.
+                </div>
+                <div class="product-learn-more">Learn More</div>
+                <button class="product-btn dome-btn">TRY FOR FREE</button>
+              </div>
             </div>
-            <div class="product-learn-more">Learn More</div>
-            <button class="product-btn dome-btn">TRY FOR FREE</button>
-          </div>
-        </div>
+          </slide>
+          <hooper-navigation slot="hooper-addons"></hooper-navigation>
+          <hooper-pagination slot="hooper-addons"></hooper-pagination>
+        </hooper>
       </div>
+      <!-- Slider Stopped -->
     </div>
     <WhyCloudenly />
     <div class="high-growth-companies sm:flex justify-center">
@@ -139,12 +151,17 @@
 </template>
 
 <script>
+import { Hooper, Slide, Navigation as HooperNavigation } from 'hooper'
 import WhyCloudenly from '../components/contents/WhyCloudenly'
 import ProductPackComponent from '../components/contents/ProductPackComponent'
+import 'hooper/dist/hooper.css'
 export default {
   components: {
     WhyCloudenly,
     ProductPackComponent,
+    Hooper,
+    Slide,
+    HooperNavigation,
   },
   data() {
     return {
