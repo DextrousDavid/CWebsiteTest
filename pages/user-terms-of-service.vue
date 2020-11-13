df1c3e9c232313b2e6824e3faa17b30c76ba631f
Ohelebo <ohelebo@gmail.com>
2020-11-13 22:02:56 +0100

    user-terms-of-service page

---
+++ pages/user-terms-of-service.vue
@@ -0,0 +1,305 @@
+<template>
+  <no-ssr>
+    <div>
+      <AppHeader />
+      <!-- <Banner /> -->
+      <section class="banner mb-8">
+        <div class="content-center mr-3 ml-3">
+          <h1 class="banner-h1">Terms and Policies</h1>
+          <div style="max-width: 550px; margin: auto"></div>
+        </div>
+      </section>
+      <!-- Banner Ends here -->
+      <main class="container mx-auto px-6 py-10 mt-8">
+        <div class="flex flex-wrap -mx-6">
+          <div class="side-bar w-xs px-6">
+            <div class="flex-wrap bg-white">
+              <a
+                href="#"
+                class="block no-underline font-semibold text-grey-dark px-4 py-1"
+                >ON THIS PAGE</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >Customer Terms of Service</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >User Terms of Service</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >Customer-Specific Suppl.</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >API Terms of Service</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >App Directory Agreement</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >Service Level Agreement</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >App Directory Agreement</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >Data Processing Agenda</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >Slack Partner Program Terms & Condition</a
+              >
+
+              <a
+                href="#"
+                class="block no-underline font-semibold text-grey-dark px-4 py-1 mt-5"
+                >ON THIS PAGE</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >Privacy Shield Notice</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >Acceptable Use Policy</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >Slack Subprocessors</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >Slack Affiliates</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >DMCA Policy</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >Multi-year Accessibility Plan</a
+              >
+              <a
+                href="#"
+                class="mt-5 block no-underline font-semibold text-grey-dark px-4 py-1"
+                >SECURITY</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >Security Practices</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >Report a Vulnerability</a
+              >
+              <a
+                href="#"
+                class="mt-5 block no-underline font-semibold text-grey-dark px-4 py-1"
+                >SECURITY</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >Cloudenly Certified Participant</a
+              >
+              <a
+                href="#"
+                class="block no-underline text-grey-dark px-4 py-1 text-sm"
+                >Agreement</a
+              >
+              <!-- <a
+              href="#"
+              class="block no-underline text-grey-dark px-4 py-1 text-sm"
+              >Slack Partner Program Terms and Conditions</a
+            > -->
+            </div>
+          </div>
+
+          <!-- Resources Items starts here -->
+          <div class="flex-1 flex-wrap">
+            <!-- Row One Starts here! -->
+            <div class="flex mb-6">
+              <div class="px-4 w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2">
+                <nuxt-link to="/faq">
+                  <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                    <div class="font-bold text-lg mb-2 pl-5 pt-5">
+                      User Terms of Service
+                    </div>
+                    <div class="pb-5 pl-5">
+                      1Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Amet, iusto!
+                    </div>
+                    <img
+                      class="w-full mb-3"
+                      src="~assets/policyIcons/security.png"
+                      alt="security card"
+                    />
+                    <div class="float-left pl-5 pb-4">
+                      <a href="#" class="text-blue-500">READ MORE</a>
+                    </div>
+                    <div class="float-right pb-4 pr-5"></div>
+                  </div>
+                </nuxt-link>
+              </div>
+
+              <div class="px-4 w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2">
+                <nuxt-link to="/faq">
+                  <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                    <div class="font-bold text-lg mb-2 pl-5 pt-5">
+                      User Terms of Service
+                    </div>
+                    <div class="pb-5 pl-5">
+                      2Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Amet, iusto!
+                    </div>
+                    <img
+                      class="w-full mb-3"
+                      src="~assets/policyIcons/secured-lock.png"
+                      alt="secured lock card"
+                    />
+                    <div class="float-left pl-5 pb-4">
+                      <a href="#" class="text-blue-500">READ MORE</a>
+                    </div>
+                    <div class="float-right pb-4 pr-5"></div>
+                  </div>
+                </nuxt-link>
+              </div>
+            </div>
+
+            <div class="flex mb-4">
+              <div class="px-4 w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2">
+                <nuxt-link to="/faq">
+                  <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                    <div class="font-bold text-lg mb-2 pl-5 pt-5">
+                      Security Overview
+                    </div>
+                    <div class="pb-5 pl-5">
+                      3Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Amet, iusto!
+                    </div>
+                    <img
+                      class="w-full mb-3"
+                      src="~assets/policyIcons/padlock.png"
+                      alt="security card"
+                    />
+                    <div class="float-left pl-5 pb-4">
+                      <a href="#" class="text-blue-500">READ MORE</a>
+                    </div>
+                    <div class="float-right pb-4 pr-5"></div>
+                  </div>
+                </nuxt-link>
+              </div>
+              <div class="px-4 w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2">
+                <nuxt-link to="/faq">
+                  <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                    <div class="font-bold text-lg mb-2 pl-5 pt-5">
+                      Acceptable use policy
+                    </div>
+                    <div class="pb-5 pl-5">
+                      4Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Amet, iusto!
+                    </div>
+                    <img
+                      class="w-full mb-3"
+                      src="~assets/policyIcons/folder.png"
+                      alt="security card"
+                    />
+                    <div class="float-left pl-5 pb-4">
+                      <a href="#" class="text-blue-500">READ MORE</a>
+                    </div>
+                    <div class="float-right pb-4 pr-5"></div>
+                  </div>
+                </nuxt-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  </no-ssr>
+</template>
+
+<script>
+import AppHeader from './../components/header/AppHeader'
+export default {
+  components: {
+    AppHeader,
+  },
+}
+</script>
+
+<style scoped>
+.banner-h1 {
+  margin-top: -50px;
+  margin-bottom: 10px;
+  padding: 10px 0;
+  font-size: 36px;
+  vertical-align: top;
+  color: #fff;
+}
+
+.banner-p {
+  font-size: 15px;
+  text-align: center;
+  margin: auto;
+  color: #fff;
+}
+
+.banner {
+  max-width: 100%;
+  padding: 8rem 0 7rem 0;
+  margin-top: 5rem;
+  text-align: center;
+  background-image: linear-gradient(269.56deg, #47c9fa 15.32%, #4881da 79.59%);
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  color: white;
+}
+
+@media (max-width: 728px) {
+  /* .your-subscription-code {
+        width: 10%;
+    } */
+  .add-more-attachments-hr {
+    display: none;
+  }
+  .banner-p {
+    font-size: 14px;
+  }
+
+  .banner-h1 {
+    font-size: 28px;
+  }
+
+  .banner {
+    background-image: linear-gradient(
+      269.56deg,
+      #47c9fa 15.32%,
+      #4881da 79.59%
+    );
+  }
+}
+</style>
